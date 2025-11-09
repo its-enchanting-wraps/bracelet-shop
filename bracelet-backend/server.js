5c25b94e0412624f1fbfed2977d4b47c28c1faaf
its-enchanting-wraps <its.enchanting.wraps@gmail.com>
2025-11-09 05:40:23 +0530

    Update server.js

--- bracelet-backend/server.js
+++ bracelet-backend/server.js
@@ -25,4 +25,10 @@ app.get("/products", async (req, res) => {
   res.json(products);
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
